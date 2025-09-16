69755729845d1d20a96faa08b5d98fe3a9974007
ilham-sh <ilhamsyauqih@outlook.co.id>
2025-09-16 11:49:50 +0700

    Update akhir fitur simpan local storage dan tambahan header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
 import Form from './components/Form'
 import ToDoList from './components/ToDoList'
 
 function App() {
   const listBaru = useRef('');
-  const [listBanyakk, setList] = useState([])
-  console.log(listBanyakk);
+  const STORAGE = 'TODOLIST_APP'
+  const [listBanyakk, setList] = useState(()=>{
+    return JSON.parse(localStorage.getItem(STORAGE)) || []
+  })
+
+  const [listSelesai, setListSelesai] = useState(0)
+
+  useEffect(()=>{
+    localStorage.setItem(STORAGE, JSON.stringify(listBanyakk))
+    const selesai = listBanyakk.filter((item)=>item.completed == true).length
+    setListSelesai(selesai)
+  },[listBanyakk])
   
   function pindahList(indexSekarang, updateIndex) {
     const dataSekarang = listBanyakk[indexSekarang] //Data dengan id ke 1 yakni syauqi
@@ -63,7 +73,7 @@ function App() {
   return (
     <div>
       <>
-        <Form addTask={addTask} listBaru={listBaru} />
+        <Form addTask={addTask} listBaru={listBaru} listSelesai={listSelesai} listBanyakk={listBanyakk}/>
         <ToDoList listBanyakk={listBanyakk} tandaSelesai={tandaSelesai} pindahList={pindahList} hapus={hapus}/>
       </>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ const Form = (props) => {
         <div>
             <div className="wrapper">
                 <header>
-                    <h3>🔰 TODOLIST </h3>
+                    <h3>🔰 TODOLIST </h3><span>{props.listSelesai || '0'} / {props.listBanyakk.length}</span>
                 </header>
                 <form className="input-box">
                     <input type="text" ref={props.listBaru} placeholder="Add Your Task" />
